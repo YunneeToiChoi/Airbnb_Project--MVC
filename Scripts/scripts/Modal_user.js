fe380176378c522fe76ea699a0313c1c30a9ff92
Phan Văn Năng <phanvannang004@gmail.com>
2023-10-13 22:40:05 +0700

    FINISHED FORM LOGIN VA REGISTER

--- Scripts/scripts/Modal_user.js
+++ Scripts/scripts/Modal_user.js
@@ -21,6 +21,7 @@ overplayUser.addEventListener('click', remove); // tat nav user o ngoai main
 ////////////////////////////  END On Off USERNAV     //////////////////////////
 
 const registerNav = document.querySelector('.user-header__form--navRegister') // goi sign in
+const loginNav = document.querySelector('.user-header__form--navLogin') //Bat form login nav
 const ModalForm = document.querySelector('.form-vadition--modal') // goi form
 const closeForm = ModalForm.querySelector('.vadition__header-icon') // goi dong form
 const overPlayForm = ModalForm.querySelector('.form-vadition--overplay') // goi overplay
@@ -29,8 +30,14 @@ const overPlayForm = ModalForm.querySelector('.form-vadition--overplay') // goi 
 
 ////////////////////////////////// BEGIN OPEN OFF form///////////////////////////
 
-registerNav.addEventListener('click', () => { // mo form
-    ModalForm.classList.add('active')
+registerNav.addEventListener('click', () => { // mo form REGISTER
+    ModalForm.classList.add('active') // nhan nav register >> mo form >> + active form register
+    registerForm.classList.add('active')        // active form register
+    hideBoxNavForm();
+})
+loginNav.addEventListener('click', () => { // mo form LOGIN
+    ModalForm.classList.add('active') // nhan nav login >> mo form >> + active form login
+    loginForm.classList.add('active')        // active form register
     hideBoxNavForm();
 })
 
@@ -41,10 +48,20 @@ function hideBoxNavForm() { // dong form
 
 // close Form = overplay
 
-overPlayForm.addEventListener('click', () => { ModalForm.classList.remove('active') }) // click overplay >> tat form
-closeForm.addEventListener('click', () => { ModalForm.classList.remove('active') }) // icon close
+overPlayForm.addEventListener('click', () => { ModalForm.classList.remove('active'); registerForm.classList.remove('active');loginForm.classList.remove('active') }) // click overplay >> tat form >> sau do tat register/login active 
+closeForm.addEventListener('click ', () => { ModalForm.classList.remove('active'); registerForm.classList.remove('active'); loginForm.classList.remove('active') })// icon closeForm*/
+
+
 
 
 ////////////////////////////////// END OPEN OFF form///////////////////////////
+
+
+////FORM NAV TO LOGIN/REGISTER //
+const loginForm = ModalForm.querySelector('.form-vadition__container--login') // bat form login
+const registerForm = ModalForm.querySelector('.form-vadition__container--register') // bat form login
+
+
+
 
 
